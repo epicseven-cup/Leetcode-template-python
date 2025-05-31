1a9249fce08ea0eb53e5c6e86afc693a649490b0
epicsven-cup <jackylin616@gmail.com>
2025-05-31 00:52:36 -0400

    update template structure

---
+++ solution.py
@@ -0,0 +1,13 @@
+"""
+Copy and past LeetCode starter code here
+
+Example:
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return []
+"""
+
+
+class Solution:
+    pass
---
+++ tests/test_solution.py
@@ -0,0 +1,20 @@
+import unittest
+from solution import Solution
+
+class TestSolution(unittest.TestCase):
+    """
+    Create and add default LeetCode testcase that is provided to you
+
+    Example:
+         # TwoSum default testcase
+        def test_testcase1(self):
+            # Inputs
+            nums = [2,7,11,15]
+            target = 9
+            self.assertEqual(
+                Solution.twoSum( nums, target ),
+                [0, 1]
+            )
+    """
+    def test_testcase1(self):
+        pass
